201211e43ca2edf0d99c444f1f0edef5e9bdfb32
Helen <helenyarinovskaya@gmail.com>
2017-01-31 00:44:09 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+    	this.maxValue = max;
     }
 
     guess() {
-
+        this.middle = Math.ceil((this.maxValue + this.minValue)/2);
+    	return this.middle;   	
     }
 
     lower() {
-
+        this.minValue = this.middle;        
     }
 
     greater() {
-
+        this.maxValue = this.middle; 
     }
 }
 
